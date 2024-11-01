6daf0ea2d5b4c1c54b6f416fca3a05471f2c3ccb
Teikaraku <teikaraku@icloud.com>
2024-11-02 01:08:59 +0800

    Site updated: 2024-11-02 01:08:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/11/游戏工具/","2024/10/11/新人三百问/"];function toRandomPost(){
+var posts=["2024/10/11/新人三百问/","2024/10/11/游戏工具/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
